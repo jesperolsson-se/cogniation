bf09342f1068a70201e8c808ef4af699842ff60e
Jesper Olsson <software@jesperolsson.se>
2021-08-17 19:03:14 +0200

    #4 Hello world

---
+++ src/main/java/se/jesperolsson/cogniation/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Cogniation is licensed under GPL-3.0. More info is found in ${basedir}/LICENSE.
+ */
+
+/**
+ * Cogniation.
+ *
+ * Tool for cognitive association training/rehabilitation.
+ */
+package se.jesperolsson.cogniation;
---
+++ src/test/java/se/jesperolsson/cogniation/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * Cogniation is licensed under GPL-3.0. More info is found in ${basedir}/LICENSE.
+ */
+
+/**
+ * Tests for Cogniation.
+ */
+package se.jesperolsson.cogniation;

c39ed0f77f52979242cacab5786b12d9a2fddfbc
Jesper Olsson <software@jesperolsson.se>
2021-09-13 22:20:32 +0200

    #11 Introduce Exercises

---
+++ src/main/java/se/jesperolsson/cogniation/Exercise.java
@@ -0,0 +1,41 @@
+/*
+ * Cogniation is licensed under GPL-3.0. More info is found in ${basedir}/LICENSE.
+ */
+package se.jesperolsson.cogniation;
+
+import java.util.Iterator;
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+import org.takes.rs.RsText;
+
+/**
+ * Exercise for cognitive training or rehabilitation.
+ *
+ * An exercise is a series of association tasks that can be presented to the user.
+ *
+ * @since 0.1
+ */
+public final class Exercise implements Take {
+
+    /**
+     * The associations comprising the exercise.
+     */
+    private final Iterator<Association> associations;
+
+    /**
+     * Ctor.
+     *
+     * @param associations A series of association tasks.
+     */
+    public Exercise(final Iterator<Association> associations)  {
+        this.associations = associations;
+    }
+
+    @Override
+    public Response act(final Request request) {
+        return new RsText(
+            this.associations.next().instructions()
+        );
+    }
+}
---
+++ src/test/java/se/jesperolsson/cogniation/ExerciseTest.java
@@ -0,0 +1,36 @@
+/*
+ * Cogniation is licensed under GPL-3.0. More info is found in ${basedir}/LICENSE.
+ */
+package se.jesperolsson.cogniation;
+
+import java.io.IOException;
+import org.cactoos.iterator.IteratorOf;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.takes.rq.RqFake;
+import org.takes.rs.RsPrint;
+
+/**
+ * Tests for {@link Exercise}.
+ *
+ * @since 0.1
+ */
+public class ExerciseTest {
+
+    @Test
+    public void completeSeries() throws IOException {
+        final String response = "Foo";
+        Assertions.assertEquals(
+            response,
+            new RsPrint(
+                new Exercise(
+                    new IteratorOf(
+                        (Association) () -> response
+                    )
+                ).act(
+                    new RqFake("GET", "/")
+                )
+            ).printBody()
+        );
+    }
+}
